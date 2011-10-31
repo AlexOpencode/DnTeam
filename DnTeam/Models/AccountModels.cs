a24c8f3394746f8b76a65adb42dba56eb1c9a9cb
Alex Opencode <p04t@live.com>
2011-10-31 17:53:12 +0200

    commit

--- DnTeam/Models/AccountModels.cs
+++ DnTeam/Models/AccountModels.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web.Mvc;
-using DnTeam.Attributes;
 using DnTeamData;
 using DnTeamData.Models;
 
