4debabd2351e8f563db934025e6c262cf9225f6b
Alex Opencode <p04t@live.com>
2011-12-12 16:14:04 +0200

    Conversion methods added (and unit tests for them)

--- DnTeam.Tests/CommonTest.cs
+++ DnTeam.Tests/CommonTest.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel;
-using System.Linq;
+﻿using System.Linq;
 using DnTeamData;
+using DnTeamData.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using MongoDB.Bson;
 
 namespace DnTeam.Tests
 {
@@ -11,13 +12,14 @@ namespace DnTeam.Tests
     ///This is a test class for CommonTest and is intended
     ///to contain all CommonTest Unit Tests
     ///</summary>
-    [TestClass()]
+    [TestClass]
     public class CommonTest
     {
+
         /// <summary>
         ///A test for SplitValues
         ///</summary>
-        [TestMethod()]
+        [TestMethod]
         public void SplitValuesTest()
         {
             const string values = "~value1~value2~~value3"; 
@@ -25,6 +27,116 @@ namespace DnTeam.Tests
             List<string> actual = Common.SplitValues(values).ToList();
 
             Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+
+        #region GetTypedPropertyValue tests
+        
+        /// <summary>
+        /// DateTime
+        /// </summary>
+        [TestMethod]
+        public void GetTypedPropertyValueDateTimeTest()
+        {
+            const string name = "DoB";
+            string value = DateTime.Now.ToString();
+            Type type = typeof(Person);
+            dynamic expectedValue = DateTime.Parse(value);
+            dynamic actualValue;
+
+            var actual = Common.GetTypedPropertyValue(name, value, type, out actualValue);
+
+            Assert.AreEqual(true, actual);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        /// <summary>
+        /// ObjectId
+        /// </summary>
+        [TestMethod]
+        public void GetTypedPropertyValueObjectIdTest()
+        {
+            const string name = "LocatedIn";
+            dynamic expectedValue = ObjectId.GenerateNewId();
+            string value = expectedValue.ToString();
+            Type type = typeof(Person);
+            dynamic actualValue;
+
+            var actual = Common.GetTypedPropertyValue(name, value, type, out actualValue);
+
+            Assert.AreEqual(true, actual);
+            Assert.AreEqual(expectedValue, actualValue);
+
+        }
+
+        /// <summary>
+        /// List[ObjectId]
+        /// </summary>
+        [TestMethod]
+        public void GetTypedPropertyValueListObjectIdTest()
+        {
+            const string name = "OtherPeers";
+            dynamic expectedValue = ObjectId.GenerateNewId();
+            string value = expectedValue.ToString();
+            Type type = typeof(Person);
+            dynamic actualValue;
+
+            var actual = Common.GetTypedPropertyValue(name, value, type,out actualValue, true);
+
+            Assert.AreEqual(true, actual);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        /// <summary>
+        /// List[string]
+        /// </summary>
+        [TestMethod]
+        public void GetTypedPropertyValueListStringTest()
+        {
+            const string name = "Links";
+            const string expectedValue = "link";
+            Type type = typeof(Person);
+            dynamic actualValue;
+
+            var actual = Common.GetTypedPropertyValue(name, expectedValue, type, out actualValue, true);
+
+            Assert.AreEqual(true, actual);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+
+        #endregion
+
+        /// <summary>
+        ///A test for TryParseNullableDate
+        ///</summary>
+        [TestMethod]
+        public void TryParseNullableDateTest()
+        {
+            //Empty string-----------//
+            string text = string.Empty; 
+            DateTime? date;
+
+            bool actual = Common.TryParseNullableDate(text, out date);
+            
+            Assert.AreEqual(null, date);
+            Assert.AreEqual(true, actual);
+
+            //Valid DateTime-------------//
+            text = DateTime.Now.ToString();
+            DateTime? dateExpected = DateTime.Parse(text);
+            
+            actual = Common.TryParseNullableDate(text, out date);
+            
+            Assert.AreEqual(dateExpected, date);
+            Assert.AreEqual(true, actual);
+
+            //Invalid DateTime-------------//
+            text = "foo date";
+
+            actual = Common.TryParseNullableDate(text, out date);
+
+            Assert.AreEqual(null, date);
+            Assert.AreEqual(false, actual);
         }
     }
 }
--- DnTeamModel/Common.cs
+++ DnTeamModel/Common.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using MongoDB.Bson;
 
 namespace DnTeamData
 {
@@ -9,6 +11,16 @@ namespace DnTeamData
     /// </summary>
     public static class Common
     {
+        private static string GetPropertyType(Type type, string propertyName, out PropertyInfo info, bool isList)
+        {
+            info = type.GetProperty(propertyName);
+            var propType = isList ? info.PropertyType.GetGenericArguments()[0] : info.PropertyType;
+            
+            return propType.Name == "Nullable`1" 
+                ? string.Format("{0}?", info.PropertyType.GetGenericArguments()[0].Name) 
+                : propType.Name;
+        }
+
         /// <summary>
         /// Splits string of values separated by "~"
         /// </summary>
@@ -18,5 +30,106 @@ namespace DnTeamData
         {
             return values.Split('~').Where(o => !String.IsNullOrEmpty(o)).Select(o => o.Trim());
         }
+
+        /// <summary>
+        /// Returns the object's ptoperty value of the valid type
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="value">Property value</param>
+        /// <param name="type">Type of the object</param>
+        /// <param name="val">Object's ptoperty value of the valid type</param>
+        /// <param name="isList">Defines whether to get value of the [T] in the list</param>
+        /// <returns>True - if value is converted successfuly</returns>
+        public static dynamic GetTypedPropertyValue(string name, string value, Type type, out dynamic val, bool isList = false)
+        {
+            PropertyInfo info;
+            return GetTypedPropertyValue(name, value, type, out info, out val, isList);
+        }
+
+        /// <summary>
+        /// Returns the object's ptoperty value of the valid type
+        /// </summary>
+        /// <param name="name">Property name</param>
+        /// <param name="value">Property value</param>
+        /// <param name="type">Type of the object</param>
+        /// <param name="info">PropertyInfo of the value</param>
+        /// <param name="val">Object's ptoperty value of the valid type</param>
+        /// <param name="isList">Defines whether to get value of the [T] in the list</param>
+        /// <returns>True - if value is converted successfuly</returns>
+        public static bool GetTypedPropertyValue(string name, string value, Type type, out PropertyInfo info, out dynamic val, bool isList = false)
+        {
+            switch (GetPropertyType(type, name, out info, isList))
+            {
+                case "string":
+                    val = value;
+                    return true;
+
+                case "ObjectId":
+                    ObjectId objectId;
+                    if (ObjectId.TryParse(value, out objectId))
+                    {
+                        val = objectId;
+                        return true;
+                    }
+                    val = null;
+                    return false;
+
+                case "DateTime":
+                    DateTime dateTime;
+                    if (DateTime.TryParse(value, out dateTime))
+                    {
+                        val = dateTime;
+                        return true;
+                    }
+                    val = null;
+                    return false;
+                
+                case "DateTime?":
+                    DateTime? dateTimeNullable;
+                    if (TryParseNullableDate(value, out dateTimeNullable))
+                    {
+                        val = dateTimeNullable;
+                        return true;
+                    }
+                    val = null;
+                    return false;
+
+                case "decimal":
+                    decimal dcml;
+                    if (decimal.TryParse(value, out dcml))
+                    {
+                        val = dcml;
+                        return true;
+                    }
+                    val = null;
+                    return false;
+
+                default:
+                    val = value;
+                    return true;
+            }
+            
+        }
+
+        /// <summary>
+        /// Parses nullable date. If string is empty returns true and emty date
+        /// </summary>
+        /// <param name="text">Text to be parsed</param>
+        /// <param name="nDate">Output DateTime?</param>
+        /// <returns>True - if parsed. </returns>
+        public static bool TryParseNullableDate(string text, out DateTime? nDate)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                nDate = new DateTime?();
+                return true;
+            }
+
+            DateTime date;
+            bool isParsed = DateTime.TryParse(text, out date);
+            nDate = isParsed ? date : new DateTime?();
+            return isParsed;
+        }
+
     }
 }
