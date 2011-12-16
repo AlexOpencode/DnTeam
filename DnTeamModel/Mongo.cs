d88c11851243ff3c2f63eceb19360fa7f6ac1e61
Alex Opencode <p04t@live.com>
2011-12-16 16:39:33 +0200

    Comments added

--- DnTeamModel/Mongo.cs
+++ DnTeamModel/Mongo.cs
@@ -2,15 +2,27 @@
 
 namespace DnTeamData
 {
+    /// <summary>
+    /// Decribes MongoDb object
+    /// </summary>
     public static class Mongo
     {
-
+        /// <summary>
+        /// Returns MongoDatabase object with default settings
+        /// </summary>
+        /// <returns>MongoDatabase object</returns>
         static public MongoDatabase Init()
         {
             return Init(Properties.Settings.Default.DatabaseName, 
                 Properties.Settings.Default.ConnectionString);
         }
 
+        /// <summary>
+        /// Returns MongoDatabase object with defined database and settings
+        /// </summary>
+        /// <param name="databaseName">Database name</param>
+        /// <param name="connectionString">Connection string</param>
+        /// <returns></returns>
         static public MongoDatabase Init(string databaseName, string connectionString)
         {
             MongoServer server = MongoServer.Create(connectionString);
